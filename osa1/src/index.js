7a74e70a16074c3359d2da3757f587756014f429
Janzu93 <janne.lofhjelm@gmail.com>
2018-01-24 13:59:28 +0200

    Osa 1.4

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -3,26 +3,29 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     const kurssi = 'Half Stack -sovelluskehitys'
-    const osa1 = {
-      nimi: 'Reactin perusteet',
-      tehtavia: 10
-    }
-    const osa2 = {
-      nimi: 'Tiedonvälitys propseilla',
-      tehtavia: 7
-    }
-    const osa3 = {
-      nimi: 'Komponenttien tila',
-      tehtavia: 14
-    }
+    const osat = [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
   
     return (
       <div>
-          <p>{kurssi}</p>
-          <p>{osa1.nimi} {osa1.tehtavia}</p>
-          <p>{osa2.nimi} {osa2.tehtavia}</p>
-          <p>{osa3.nimi} {osa3.tehtavia}</p>
-          <p>Yhteensä {osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} tehtävää</p>
+        <h1>{kurssi}</h1>
+        <p>{osat[0].nimi} {osat[0].tehtavia}</p>
+        <p>{osat[1].nimi} {osat[1].tehtavia}</p>
+        <p>{osat[2].nimi} {osat[2].tehtavia}</p>
+        <p>Yhteensä {osat[0].tehtavia+osat[1].tehtavia+osat[2].tehtavia} tehtävää</p>
+
       </div>
     )
   }
