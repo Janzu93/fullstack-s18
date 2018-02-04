b80f202c34a903db25a72dfe805453bb307be50e
Janzu93 <janne.lofhjelm@gmail.com>
2018-02-04 16:36:27 +0200

    2.6

--- osa02/2/src/components/App.js
+++ osa02/2/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -11,20 +12,38 @@ class App extends React.Component {
     }
   }
 
+  addName = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: this.state.newName
+    }
+
+    const persons = this.state.persons.concat(personObject)
+
+    this.setState({
+      persons: persons,
+      newName: ''
+    })
+  }
+
+  handleNameChange = (event) => {
+    this.setState({ newName: event.target.value })
+  }
+
   render() {
     return (
       <div>
         <h2>Puhelinluettelo</h2>
-        <form>
+        <form onSubmit={this.addName}>
           <div>
-            nimi: <input />
+            nimi:  <input value={this.state.newName} onChange={this.handleNameChange} />
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
         <h2>Numerot</h2>
-        ...
+        <ul>{this.state.persons.map(person => <div key={person.name}>{person.name}</div>)}</ul>
       </div>
     )
   }
