bc24a305f5dcd062ab8e2b223bb9bd1eb2e7a307
Janzu93 <janne.lofhjelm@gmail.com>
2018-02-04 18:07:33 +0200

    2.8

--- osa02/2/src/components/App.js
+++ osa02/2/src/components/App.js
@@ -6,29 +6,38 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        {
+          name: 'Arto Hellas',
+          number: '0401231234'
+        }
       ],
-      newName: ''
+      newName: '',
+      newNumber: ''
     }
   }
 
   addName = (event) => {
     event.preventDefault()
     const personObject = {
-      name: this.state.newName
+      name: this.state.newName,
+      number: this.state.newNumber
     }
 
-    const persons = this.state.persons.find(p => p.name === personObject.name)?
-    this.state.persons : this.state.persons.concat(personObject)
+    const persons = this.state.persons.find(p => p.name === personObject.name) ?
+      this.state.persons : this.state.persons.concat(personObject)
 
     this.setState({
       persons,
-      newName: ''
+      newName: '',
+      newNumber: ''
     })
   }
 
   handleNameChange = (event) => {
     this.setState({ newName: event.target.value })
+  }
+  handleNumberChange = (event) => {
+    this.setState({ newNumber: event.target.value })
   }
 
   render() {
@@ -40,11 +49,14 @@ class App extends React.Component {
             nimi:  <input value={this.state.newName} onChange={this.handleNameChange} />
           </div>
           <div>
+            numero: <input value={this.state.newNumber} onChange={this.handleNumberChange} />
+          </div>
+          <div>
             <button type="submit">lisää</button>
           </div>
         </form>
         <h2>Numerot</h2>
-        <ul>{this.state.persons.map(person => <div key={person.name}>{person.name}</div>)}</ul>
+        <ul>{this.state.persons.map(person => <div key={person.name}>{person.name} {person.number}</div>)}</ul>
       </div>
     )
   }
