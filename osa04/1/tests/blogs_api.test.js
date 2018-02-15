35fceadbcde65335209b1317fcf49000b3e902e8
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2018-02-15 22:34:17 +0200

    4.9

--- osa04/1/tests/blogs_api.test.js
+++ osa04/1/tests/blogs_api.test.js
@@ -3,6 +3,10 @@ const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
+beforeAll(async () => {
+  await Blog.remove({})
+})
+
 describe('Test GET', () => {
   test('blogs are returned as JSON', async () => {
     await api
@@ -13,19 +17,40 @@ describe('Test GET', () => {
 })
 
 describe('Test POST', () => {
-  const newBlog =  {
+  const newBlog = {
     title: "Reaction patterns",
     author: "Michael Changgg",
     url: "https://reactpatterns.com.ion/",
     likes: 0
   }
 
+  const newBlogNoLikes = {
+    title: "Reaction patterns",
+    author: "Michael Changgg",
+    url: "https://reactpatterns.com.ion/"
+  }
+
   test('blogs are added when POST', async () => {
     await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('Blogs without likes get added with 0 likes', async () => {
+    await api
+      .post('/api/blogs')
+      .send(newBlogNoLikes)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const res = await api
+    .get('/api/blogs')
+    .expect(200)
     .expect('Content-Type', /application\/json/)
+    
+    expect(res.body[0].likes).toBe(0)
   })
 })
 afterAll(() => {
