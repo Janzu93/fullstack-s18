36fea3b1f5bcf325260f2520fe8547e0c4076963
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2018-02-15 21:40:11 +0200

    configs for production, devel and test

--- osa04/1/index.js
+++ osa04/1/index.js
@@ -5,21 +5,17 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogsRouter')
-
-if ( process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+const config = require('./utils/config')
 
 app.use(cors())
 app.use(bodyParser.json())
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.connect(config.mongoUrl)
 mongoose.Promise = global.Promise
 
 app.use('/api/blogs', blogsRouter)
 
-const PORT = 3003
+const PORT = config.port
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
