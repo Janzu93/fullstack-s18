9289b852a7eed6975cdd46202fea80bec1f3373e
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2018-02-25 18:00:56 +0200

    4.20

--- osa04/1/index.js
+++ osa04/1/index.js
@@ -9,12 +9,19 @@ const usersRouter = require('./controllers/usersRouter')
 const loginRouter = require('./controllers/login')
 const config = require('./utils/config')
 
+const getTokenFrom = (request, response, next) => {
+  request.body.token = request.headers.authorization.substring(7)
+  next()
+}
+
+
 app.use(cors())
 app.use(bodyParser.json())
 
 mongoose.connect(config.mongoUrl)
 mongoose.Promise = global.Promise
 
+app.use(getTokenFrom)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
@@ -26,8 +33,8 @@ server.listen(config.port, () => {
 })
 server.on('close', () => {
   mongoose.connection.close()
-}) 
+})
 
 module.exports = {
-  app, server 
+  app, server
 }
