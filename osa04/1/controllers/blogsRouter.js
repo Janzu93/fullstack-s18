d67ce5d379c79a400b64657521332de23daf5baf
Janne Löfhjelm <janne.lofhjelm@gmail.com>
2018-02-25 17:08:38 +0200

    4.19

--- osa04/1/controllers/blogsRouter.js
+++ osa04/1/controllers/blogsRouter.js
@@ -2,6 +2,17 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user')
   response.json(blogs)
@@ -11,8 +22,14 @@ blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
   try {
-    const users = await User.find({})
-    const user = users[0]
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
     const blog = new Blog({
       title: body.title,
       author: body.author,
@@ -32,8 +49,12 @@ blogsRouter.post('/', async (request, response) => {
 
     response.status(201).json(blog)
   } catch (exception) {
-    console.log(exception)
-    response.status(500).json({ error: 'something went wrong...' })
+    if (exception.name === 'JsonWebTokenError') {
+      response.status(401).json({ error: exception.message })
+    } else {
+      console.log(exception)
+      response.status(500).json({ error: 'something went wrong...' })
+    }
   }
 
 })
